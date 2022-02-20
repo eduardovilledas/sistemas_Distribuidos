fa562c99115863d8cc5054d3d5ed4e301dec2884
Alonso Bernal Servin <abernals1501@alumno.ipn.mx>
2022-02-19 18:36:51 -0600

    Add files via upload

---
+++ Socket_Cliente.h
@@ -0,0 +1,9 @@
+
+#ifndef _SOCKET_CLIENTE_H
+#define _SOCKET_CLIENTE_H
+
+#include <sys/socket.h>
+
+int openConexion_Inet (char *Host_Servidor, char *Servicio);
+
+#endif
---
+++ Socket_Servidor.h
@@ -0,0 +1,9 @@
+
+#ifndef _SOCKET_SERVIDOR_H
+#define _SOCKET_SERVIDOR_H
+
+int acceptsConexion_Client (int Descriptor);
+
+int openSocket_Inet (char *Servicio);
+
+#endif
